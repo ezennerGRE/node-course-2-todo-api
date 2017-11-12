6beadbf81c967eff92778fd5861aff95b4fd268b
Eric Zenner <ezenner2@gmail.com>
2017-11-12 13:15:03 -0500

    delete test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,42 @@ describe ("GET /todos/id", () => {
 			.end(done);
 	});
 });
+
+describe("DELETE /todos/:id",() => {
+	it('should remove a todo', (done) => {
+		var hexId = todos[1]._id.toHexString();
+
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo._id).toBe(hexId);
+			})
+			.end((err,res) => {
+				if(err){
+					return done(err);
+				}
+
+				// Query DB using findById toNotExist
+				Todo.findById(hexId).then((todo) => {
+					expect(todo).toNotExist();
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+
+	it('should return a 404 if todo not found', (done) => {
+		var newId = new ObjectID();
+		request(app)
+			.delete(`/todos/${newId.toHexString()}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return a 404 for non-object ids', (done) => {
+		request(app)
+			.delete(`/todos/123`)
+			.expect(404)
+			.end(done);
+	});
+})
