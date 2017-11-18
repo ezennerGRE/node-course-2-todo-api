d908beb5077ad3b7676d9d41afc5fa887a6fa871
Eric Zenner <ezenner2@gmail.com>
2017-11-18 14:33:21 -0500

    add GET users/me

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {authenticate} = require('./middleware/authenticate');
 var app = express();
 var port = process.env.PORT;
 
@@ -105,10 +105,10 @@ app.patch('/todos/:id', (req,res) => {
 });
 
 
+
 // POST /users
 app.post('/users', (req,res) => {
 	var body = _.pick(req.body, ['email','password']);
-
 	var user = new User(body);
 
 	user.save().then( () => {
@@ -118,6 +118,10 @@ app.post('/users', (req,res) => {
 	}).catch( (e) => {
 		res.status(400).send(e);	
 	});
+});
+
+app.get('/users/me', authenticate, (req,res) => {
+	res.send(req.user);
 });
 
 app.listen(port, () => {
